2706e22eb9877bf0379614f797456e8d3c6db619
yoshiioko <acardoza@pdx.edu>
2022-12-15 13:08:04 -0800

    Implemented function to airdrop 1 SOL and confirm tx.

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,33 @@ import * as fs from "fs";
 import dotenv from "dotenv";
 dotenv.config();
 
+async function airdropSolIfNeeded(
+  signer: Web3.Keypair,
+  connection: Web3.Connection
+) {
+  const balance = await connection.getBalance(signer.publicKey);
+  console.log("Current balance is", balance / Web3.LAMPORTS_PER_SOL, "SOL");
+
+  if (balance / Web3.LAMPORTS_PER_SOL < 1) {
+    console.log("Airdropping 1 SOL");
+    const airDropSignature = await connection.requestAirdrop(
+      signer.publicKey,
+      Web3.LAMPORTS_PER_SOL
+    );
+
+    const latestBlockhash = await connection.getLatestBlockhash();
+
+    await connection.confirmTransaction({
+      blockhash: latestBlockhash.blockhash,
+      lastValidBlockHeight: latestBlockhash.lastValidBlockHeight,
+      signature: airDropSignature,
+    });
+
+    const newBalance = await connection.getBalance(signer.publicKey);
+    console.log("New balance is", newBalance / Web3.LAMPORTS_PER_SOL, "SOL");
+  }
+}
+
 async function initializeKeypair(
   connection: Web3.Connection
 ): Promise<Web3.Keypair> {
@@ -13,12 +40,16 @@ async function initializeKeypair(
     console.log("Creating .env file");
     fs.writeFileSync(".env", `PRIVATE_KEY=[${signer.secretKey.toString()}]`);
 
+    await airdropSolIfNeeded(signer, connection);
+
     return signer;
   }
 
   const secret = JSON.parse(process.env.PRIVATE_KEY ?? "") as number[];
   const secretKey = Uint8Array.from(secret);
   const keypairFromSecret = Web3.Keypair.fromSecretKey(secretKey);
+
+  await airdropSolIfNeeded(keypairFromSecret, connection);
 
   return keypairFromSecret;
 }
