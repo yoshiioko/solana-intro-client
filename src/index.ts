36b3273833cae472394a99a19554ce1e37920619
yoshiioko <acardoza@pdx.edu>
2022-12-15 12:47:15 -0800

    Implement function to create a keypair if a .env file doesn't exist. Else read from private key from .env file.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,41 @@
-async function main() {}
+import * as Web3 from "@solana/web3.js";
+import * as fs from "fs";
+import dotenv from "dotenv";
+dotenv.config();
+
+async function initializeKeypair(
+  connection: Web3.Connection
+): Promise<Web3.Keypair> {
+  if (!process.env.PRIVATE_KEY) {
+    console.log("Generating new keypair... 🗝️");
+    const signer = Web3.Keypair.generate();
+
+    console.log("Creating .env file");
+    fs.writeFileSync(".env", `PRIVATE_KEY=[${signer.secretKey.toString()}]`);
+
+    return signer;
+  }
+
+  const secret = JSON.parse(process.env.PRIVATE_KEY ?? "") as number[];
+  const secretKey = Uint8Array.from(secret);
+  const keypairFromSecret = Web3.Keypair.fromSecretKey(secretKey);
+
+  return keypairFromSecret;
+}
+
+async function main() {
+  const connection = new Web3.Connection(Web3.clusterApiUrl("devnet"));
+  const signer = await initializeKeypair(connection);
+
+  console.log("Public key:", signer.publicKey.toBase58());
+}
 
 main()
-    .then(() => {
-        console.log("Finished successfully")
-        process.exit(0)
-    })
-    .catch((error) => {
-        console.log(error)
-        process.exit(1)
-    })
+  .then(() => {
+    console.log("Finished successfully");
+    process.exit(0);
+  })
+  .catch((error) => {
+    console.log(error);
+    process.exit(1);
+  });
